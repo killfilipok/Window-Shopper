7dbcc226f7df809aad271c51342d56ebcaa930cc
Philip <killfilipok@gmail.com>
2019-03-02 18:56:11 +0200

    init

---
+++ window-shoper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shoper
+//
+//  Created by Philip on 3/2/19.
+//  Copyright © 2019 Philip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
